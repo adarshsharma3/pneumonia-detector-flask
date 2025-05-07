214c75f7052d66437caf036f568b2b24a89164fd
adarshsharma3 <adarshsharma300704@gmail.com>
2025-05-08 00:35:08 +0530

    Res

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,6 @@ import ImageUpload from './ImageUpload';
 function App() {
   return (
     <div className="min-h-screen w-full  bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center ">
-     
       <ImageUpload />
     </div>
   );
